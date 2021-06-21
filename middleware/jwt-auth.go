e47204349d1e6926b51468433f3d5236cb817ac1
geekbim <manyuabim9@gmail.com>
2021-06-22 05:12:47 +0700

    JWT Middleware

---
+++ middleware/jwt-auth.go
@@ -0,0 +1,38 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/geekbim/Golang-Clean-Pattern-Ceunah/helper"
+	"github.com/geekbim/Golang-Clean-Pattern-Ceunah/service"
+	"github.com/gin-gonic/gin"
+	"github.com/go-delve/delve/service"
+)
+
+// AuthorizeJWT validates the token user given, return 401 if not valid
+func AuthorizeJWT(jwtService service.JWTService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+
+		if authHeader == "" {
+			response := helper.BuildErrorResponse("Failed to process request", "No token found", nil)
+			c.AbortWithStatusJSON(http.StatusBadRequest, response)
+		}
+
+		token, err := jwtService.ValidateToken(authHeader)
+
+		if token.Valid {
+			claims := token.Claims.(jwt.MapClaims)
+			log.Println("Claim[user_id]: ", claims["user_id"])
+			log.Println("Claim[issuer]: ", claims["issuer"])
+			log.Println("Claim[exp]: ", claims["exp"])
+		} else {
+			log.Println(err)
+
+			response := helper.BuildErrorResponse("Token is not valid", err.Error(), nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+		}
+	}
+}
