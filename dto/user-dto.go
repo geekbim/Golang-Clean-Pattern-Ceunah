1e41a4cfc1290e4151c0e38cc9a1e97bfd6ca527
geekbim <manyuabim9@gmail.com>
2021-06-29 06:52:21 +0700

    fixed typo

--- dto/user-dto.go
+++ dto/user-dto.go
@@ -4,7 +4,7 @@ package dto
 type UserUpdateDTO struct {
 	ID       uint64 `json:"id" form:"id"`
 	Name     string `json:"name" form:"name" binding:"required"`
-	Email    string `json:"email" form:"email" binding:"required, email"`
+	Email    string `json:"email" form:"email" binding:"required,email"`
 	Password string `json:"password,omitempty" form:"password,omitempty"`
 }
 
